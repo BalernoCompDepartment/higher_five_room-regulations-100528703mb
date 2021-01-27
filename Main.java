94b726e95fe836b5f5a62e12979dd861822a686c
100528703mb <100528703@schools.edin.org>
2021-01-27 15:16:13 +0000

    program

--- Main.java
+++ Main.java
@@ -1,5 +1,63 @@
+import java.util.Scanner;
+
+
+class dimensions {
+  double length;
+  double width;
+  double height;
+  double airSpace;
+}
+
+
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    
+    dimensions measuredRoom = new dimensions();
+    measuredRoom = getDimensions();
+
+    int legalPeople = calculatePersons(measuredRoom);
+
+     displayNoPeople(legalPeople);
+
   }
+
+
+public static dimensions getDimensions() { 
+   dimensions localRoom = new dimensions();
+   localRoom.length = getValidReal(2.0, 20.0, "Please enter length between 2 and 20 metres");
+   localRoom.width = getValidReal(2.0, 20.0, "Please enter width between 2 and 20 metres");
+   localRoom.height = getValidReal(2.0, 10.0, "Please enter height between 2 and 10 metres");
+   localRoom.airSpace = getValidReal(5.0, 15.0, "Please enter air space between 5 and 5 meters cubed");
+      
+return localRoom;
+}
+
+public static double getValidReal (double min, double max, String msg) {
+  
+  double input;
+  input = Keyboard.getReal(msg);
+  while (input < min || input > max) {
+    System.out.println("Please enter a valid input");
+    input = Keyboard.getReal(msg);
+  }
+  
+  return input;
+}
+
+public static int calculatePersons (dimensions localRoom) {
+ 
+  double volume = localRoom.length * localRoom.width * localRoom.height;
+  double people = volume/localRoom.airSpace;
+  
+  return (int) Math.floor(people);
+
+}
+
+public static void displayNoPeople(int noPeople) {
+  System.out.println("The max number of legal people is " + noPeople);
+}
+  
+
 }
